3fc9c7cae01e3be9a9a261a9e9f31ed0f02e1a2d
Feña Agar <ferliagno@gmail.com>
2013-05-05 22:02:15 -0400

    analytics y 404

--- app.rb
+++ app.rb
@@ -1,9 +1,20 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+
+  # ROUTES
   get '/' do
     @rand_tagline = rand_tagline
     haml :"index/index", layout: true, local: @rand_tagline
+  end
+
+  not_found do
+    haml :error
+  end
+
+  # FUNCTIONS
+  def site_url
+    "http://fernandoagar.cl/"
   end
 
   private
